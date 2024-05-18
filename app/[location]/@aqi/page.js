3d5e9f602968b1f0e0f256d9eaac3fd5d503c46c
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-05-18 23:26:07 +0600

    NoLocationFound Page added

--- app/[location]/@aqi/page.js
+++ app/[location]/@aqi/page.js
@@ -1,10 +1,17 @@
 import AQIComponent from "@/components/AQIComponent";
+import NoLocationFound from "@/components/NoLocationFound";
+import { getResolveLatLon } from "@/lib/location-info";
 
-const AqiPage = ({
+const AqiPage = async ({
   params: { location },
   searchParams: { latitude, longitude },
 }) => {
-  return <AQIComponent lat={latitude} lon={longitude} />;
+  const resolved = await getResolveLatLon(location, latitude, longitude);
+  if (resolved?.lat && resolved?.lon) {
+    return <AQIComponent lat={resolved.lat} lon={resolved.lon} />;
+  } else {
+    return <NoLocationFound />;
+  }
 };
 
 export default AqiPage;
---
+++ components/NoLocationFound.jsx
@@ -0,0 +1,7 @@
+import Card from "./Card";
+
+const NoLocationFound = () => {
+  return <Card>NoLocationFound</Card>;
+};
+
+export default NoLocationFound;
